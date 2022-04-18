210d0d74d41ce78947c28f1f547d4c57c7eb576b
yashikanand <yashik.anand99@gmail.com>
2022-04-18 11:50:14 +1000

    Handling the keywords error and adding get/set functions

--- script.js
+++ script.js
@@ -3,14 +3,56 @@ var y;
 var face;
 var input;
 
-function getVal() {
+/* Function to store the input received from the form input field */
+function getInput() {
   input = document.getElementById("input").value;
-  console.log(input);
   document.getElementById("input").value = "";
 }
 
+function processInput(input) {
+  var data = input.split(" ");
+  data[0] = data[0].toUpperCase();
+  switch (data[0]) {
+    case "PLACE":
+      console.log(0);
+      setInputAcceptanceVal("Input Accepted");
+      break;
+    case "MOVE":
+      console.log(1);
+      break;
+    case "LEFT":
+      console.log(2);
+      break;
+    case "RIGHT":
+      console.log(3);
+      break;
+    case "REPORT":
+      console.log(4);
+      break;
+    default:
+      setInputAcceptanceVal("Input Ignored");
+  }
+}
+
+/* Function to show the value when submit button is clicked */
+function setInputAcceptanceVal(value) {
+  var element = document.getElementById("input-acceptance");
+  element.innerText = value;
+}
+
+/* Function to set the ouput when the report keyword is called */
+function setOutput(output) {
+  var element = document.getElementById("output-received");
+  element.innerText = output;
+}
+
+/* The start of the script which captures the input on submit button click */
 window.onload = function () {
-  document.getElementById("input-submission").onclick = function () {
-    getVal();
-  };
+  const form = document.getElementById("input-area");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    getInput();
+    processInput(input);
+  });
 };
